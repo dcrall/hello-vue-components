c799b695ed17e3e99f4106ae96bf69a54c107f20
Denny Crall <dennis-crall@uiowa.edu>
2020-03-11 11:41:33 -0500

    WIP - component-names working from build directory

--- build-utils/component-names.js
+++ build-utils/component-names.js
@@ -11,42 +11,103 @@ const walkSync = (dir, filelist = []) => {
 
     });
     return filelist;
-}
-
-// Save the build dir
-let buildDir = __dirname
-let src = path.resolve(__dirname, '../src')
-
-// Switch to the src directory, so we can build relative imports
-// e.g. sub/HelloD
-process.chdir(src)
-
-// Get all the .vue files under the src directory
-let components = walkSync('.').filter(f => /\.vue$/.test(f))
-
-// Restore cwd to the build dir
-process.chdir(buildDir)
-
-// Get the names of the components from the file names
-// Return an array of the form:
-// [ 'HelloA', 'HelloB', 'HelloC', 'sub/HelloD' ]
-components = components.map(f => f.replace(/\.\w+$/, ''))
+};
 
 /**
- * Capture both the path and component name
- * [
- *    { path: 'HelloA', name: 'HelloA' },
- *    { path: 'HelloB', name: 'HelloB' },
- *    { path: 'HelloC', name: 'HelloC' },
- *    { path: 'sub/HelloD', name: 'HelloD' }
- * ]
+ *  - Collect all files under the ../src directory
+ *  - Filter down to just the .vue files
+ *  - Parse the paths, so we can clean up the data
+ *  - Build response object with path & name attributes
+ *  - Clean up the path attribute
  */
-components = components.map(f => {
-    let componentPath = f;
-    let name = path.parse(f).name;
-    return { path: componentPath, name }
-})
+let components =
+    walkSync('../src').filter(f => /\.vue$/.test(f)).map(f => path.parse(f)).map(f => {
+        return {
+            name: f.name,
+            path: f.dir
+        }
+    }).map(f => {
+        f.path = f.path.replace('../src', '');
+        // remove any leading slashes
+        f.path = f.path.replace(/^\//gi, '');
 
-console.log(components)
+        if(f.path !== '') f.path = f.path + '/';
+        f.path = f.path + f.name;
+        return f;
+    });
 
-module.exports = components
+
+console.log(components);
+
+// Export the component description objects
+module.exports = components;
+
+
+/**
+ * For future reference, here are the individual steps of the transformation
+ * after we gather all of the components under the /src directory
+ *
+ * // Filter out all the vue components
+ * walkSync('../src').filter(f => /\.vue$/.test(f))
+ *
+ * [
+ *     '../src/HelloA.vue',
+ *     '../src/HelloB.vue',
+ *     '../src/HelloC.vue',
+ *     '../src/sub/HelloD.vue'
+ *     ]
+ *
+ *
+ * // Parse the paths for the vue components
+ * walkSync('../src').filter(f => /\.vue$/.test(f)).map(f => path.parse(f))
+ *
+ * [
+ *    {
+ *        root: '',
+ *        dir: '../src',
+ *        base: 'HelloA.vue',
+ *        ext: '.vue',
+ *        name: 'HelloA'
+ *    },
+ *    {
+ *        root: '',
+ *        dir: '../src',
+ *        base: 'HelloB.vue',
+ *        ext: '.vue',
+ *        name: 'HelloB'
+ *    },
+ *    {
+ *        root: '',
+ *        dir: '../src',
+ *        base: 'HelloC.vue',
+ *        ext: '.vue',
+ *        name: 'HelloC'
+ *    },
+ *    {
+ *        root: '',
+ *        dir: '../src/sub',
+ *        base: 'HelloD.vue',
+ *        ext: '.vue',
+ *        name: 'HelloD'
+ *    }
+ *  ]
+ *
+ *
+ *  // Simplify the results to an object of the form:
+ *  // {name: 'CompnentName', path: ''}
+ *    [
+ *      { name: 'HelloA', path: '../src' },
+ *      { name: 'HelloB', path: '../src' },
+ *      { name: 'HelloC', path: '../src' },
+ *      { name: 'HelloD', path: '../src/sub' }
+ *    ]
+ *
+ *
+ *  // Clean up the path values
+ *  [
+ *    { name: 'HelloA', path: 'HelloA' },
+ *    { name: 'HelloB', path: 'HelloB' },
+ *    { name: 'HelloC', path: 'HelloC' },
+ *    { name: 'HelloD', path: 'sub/HelloD' }
+ *  ]
+ */
